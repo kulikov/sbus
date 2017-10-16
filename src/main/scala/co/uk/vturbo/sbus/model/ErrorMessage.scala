2935c8e3da09bd9b4ef206338c843b70dfa4b5ef
kulikov <kulikov.dm@gmail.com>
2017-10-16 20:35:30 +0300

    = impr ErrorMessage: add default constructors; add MapBuilder

--- src/main/scala/co/uk/vturbo/sbus/model/ErrorMessage.scala
+++ src/main/scala/co/uk/vturbo/sbus/model/ErrorMessage.scala
@@ -13,14 +13,58 @@ class ErrorMessage(
 trait UnrecoverableFailure
 
 
-class BadRequestError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(400, msg, cause, error) with UnrecoverableFailure
-class UnauthorizedError(msg: String, cause: Throwable = null, error: String = null, val schema: Option[String] = None) extends ErrorMessage(401, msg, cause, error)
-class ForbiddenError(msg: String, cause: Throwable = null, error: String = null, _links: java.util.Map[String, Object] = null) extends ErrorMessage(403, msg, cause, error, _links)
-class NotFoundError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(404, msg, cause, error)
-class MethodNotAllowedError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(405, msg, cause, error)
-class ConflictError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(409, msg, cause, error)
-class PreconditionFailedError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(412, msg, cause, error)
-class TooManyRequestError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(429, msg, cause, error)
-class InternalServerError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(500, msg, cause, error)
-class ServiceUnavailableError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(503, msg, cause, error)
-class UnrecoverableError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(456, msg, cause, error) with UnrecoverableFailure
+class BadRequestError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(400, msg, cause, error) with UnrecoverableFailure {
+  def this(msg: String) = this(msg, null, null)
+  def this(msg: String, cause: Throwable) = this(msg, cause, null)
+}
+
+class UnauthorizedError(msg: String, cause: Throwable = null, error: String = null, val schema: Option[String] = None) extends ErrorMessage(401, msg, cause, error) {
+  def this(msg: String) = this(msg, null, null)
+  def this(msg: String, cause: Throwable) = this(msg, cause, null)
+}
+
+class ForbiddenError(msg: String, cause: Throwable = null, error: String = null, _links: java.util.Map[String, Object] = null) extends ErrorMessage(403, msg, cause, error, _links) {
+  def this(msg: String) = this(msg, null, null)
+  def this(msg: String, cause: Throwable) = this(msg, cause, null)
+}
+
+class NotFoundError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(404, msg, cause, error) {
+  def this(msg: String) = this(msg, null, null)
+  def this(msg: String, cause: Throwable) = this(msg, cause, null)
+}
+
+class MethodNotAllowedError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(405, msg, cause, error) {
+  def this(msg: String) = this(msg, null, null)
+  def this(msg: String, cause: Throwable) = this(msg, cause, null)
+}
+
+
+class ConflictError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(409, msg, cause, error) {
+  def this(msg: String) = this(msg, null, null)
+  def this(msg: String, cause: Throwable) = this(msg, cause, null)
+}
+
+class PreconditionFailedError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(412, msg, cause, error) {
+  def this(msg: String) = this(msg, null, null)
+  def this(msg: String, cause: Throwable) = this(msg, cause, null)
+}
+
+class TooManyRequestError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(429, msg, cause, error) {
+  def this(msg: String) = this(msg, null, null)
+  def this(msg: String, cause: Throwable) = this(msg, cause, null)
+}
+
+class InternalServerError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(500, msg, cause, error) {
+  def this(msg: String) = this(msg, null, null)
+  def this(msg: String, cause: Throwable) = this(msg, cause, null)
+}
+
+class ServiceUnavailableError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(503, msg, cause, error) {
+  def this(msg: String) = this(msg, null, null)
+  def this(msg: String, cause: Throwable) = this(msg, cause, null)
+}
+
+class UnrecoverableError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(456, msg, cause, error) with UnrecoverableFailure {
+  def this(msg: String) = this(msg, null, null)
+  def this(msg: String, cause: Throwable) = this(msg, cause, null)
+}
